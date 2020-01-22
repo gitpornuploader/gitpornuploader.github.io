d31c9440341644560d86b7d5d4112bf141483a38
github-flix <github-flix>
2020-01-22 12:51:27 -0300

    Add build

---
+++ build/precache-manifest.07d4479589b95a90c173a1a7cbb99a68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bddcb4e677489aed7f768e360ac0d9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48709d5036ad1e9bdcc3",
+    "url": "/static/js/2.cb5f86a0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.cb5f86a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d30f5bc161e27dc7823",
+    "url": "/static/js/main.2dcf0be9.chunk.js"
+  },
+  {
+    "revision": "b5de46d4f52da3be4dbe",
+    "url": "/static/js/runtime-main.f084aed1.js"
+  }
+]);
